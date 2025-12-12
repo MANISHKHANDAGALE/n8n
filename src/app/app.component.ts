5d18007ace9d5b3998c5c35972b3b57a7b294613
Nilesh Pandey <99646982+NileshPandey05@users.noreply.github.com>
2025-12-12 10:46:19 +0000

    add a library lucide and flowbox

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { FlowEditorComponentComponent } from "./components/flow-editor-component/flow-editor-component.component";
+import { NodeCardRenderComponent } from "./components/node-card-render/node-card-render.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, FlowEditorComponentComponent, NodeCardRenderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
